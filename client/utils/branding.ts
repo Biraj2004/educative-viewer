f8552b2e1eb66dbf7f123c87af18932bf88e8bb9
Biraj Sarkar <birajsarkar67@gmail.com>
2026-03-21 19:35:03 +0530

    improved branding

--- client/utils/branding.ts
+++ client/utils/branding.ts
@@ -1,1 +1,4 @@
-export const BRAND_ICON_URL = `/icon-og.png`;
+import iconOg from "../public/icon-og.png";
+
+// Importing the actual file bundles it into `.next/static/media`, ensuring it's included in `.next.zip`!
+export const BRAND_ICON_URL = iconOg.src;
